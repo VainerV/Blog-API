9046ad8f6ca5a897910072570563b0846ca0f410
Vadim Vainer <vainerxv@gmail.com>
2018-08-10 21:54:15 -0700

    Finalized js files, project finished

--- blogRouter.js
+++ blogRouter.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-
-const {BlogPosts} = require('/blog-posts');
+const {BlogPosts} = require('./models');
 
 
 
 // when the root of this router is called with GET, return
-// all current ShoppingList items
-router.get('/blog-posts', (req, res) => {
+// all current BlogPosts  
+router.get('/', (req, res) => {
+    console.log("Retreving Blog posts");
+    let blPost = BlogPosts.get();
+    if(blPost.length === 0){
+        console.log("No Blog posts were found");
+    }
   res.json(BlogPosts.get());
 });
 
 
-// when a new shopping list item is posted, make sure it's
-// got required fields ('name' and 'checked'). if not,
+// when a new blog is posted, make sure it's
+// got required fields ('title, content, author). if not,
 // log an error and return a 400 status code. if okay,
-// add new item to ShoppingList and return it with a 201.
-router.post('/blog-posts', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'checked'];
+// add new item to BlogPosts and return it with a 201.
+router.post('/', jsonParser, (req, res) => {
+  // ensure `title` , 'content and `author` are in request body
+  const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -30,26 +33,35 @@ router.post('/blog-posts', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = ShoppingList.create(req.body.name, req.body.checked);
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
 
 // when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-router.delete('/blog-posts/:id', (req, res) => {
-    BlogPosts.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
-  res.status(204).end();
+// try to delete that item from BlogPosts .
+router.delete('/:id', (req, res) => {
+    let blPost = BlogPosts.get(req.params.id);
+    if(blPost){
+        BlogPosts.delete(req.params.id);
+        console.log(`Deleted BlogerPost item \`${req.params.ID}\``);
+        res.status(204).end();
+    }
+    else{
+        console.log("Error: Post not found");
+        res.status(500).end();
+    }
+  
+  
 });
 
 // when PUT request comes in with updated item, ensure has
 // required fields. also ensure that item id in url path, and
 // item id in updated item object match. if problems with any
 // of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-router.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id'];
+// call `BlogPosts .update` with updated item.
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', "id"];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -68,8 +80,10 @@ router.put('/blog-posts/:id', jsonParser, (req, res) => {
   console.log(`Updating shopping list item \`${req.params.id}\``);
   const updatedItem = BlogPosts.update({
     id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
   });
   res.status(204).end();
 })
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const jsonParser = bodyParser.json();
 const app = express();
 
 // log the http layer
 app.use(morgan('common'));
 
-const bloggerRouter = require('./blog-posts');
-app.use(express.static('public'));
+const bloggerRouter = require('./blogRouter');
 
-app.use('./blog-posts', blogRouter);
+app.use(express.static('public'));   // static folder
+app.use('/blog-posts', bloggerRouter); // call for the router
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
